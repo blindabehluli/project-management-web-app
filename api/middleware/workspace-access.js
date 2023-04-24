17c9d5a01f528691874cac76b814dded1e88aca1
getoarmorina3 <bb57230@ubt-uni.net>
2023-04-24 17:49:49 +0200

    Fix workspace access middleware
    Co-Authored-By: Getoar <78223720+getoarmorina3@users.noreply.github.com>

--- api/middleware/workspace-access.js
+++ api/middleware/workspace-access.js
@@ -4,30 +4,35 @@ const { asyncHandler } = require("./async-handler");
 exports.workspaceAccess = (role) => {
   return asyncHandler(async (req, res, next) => {
     const workspaceId = req.params.workspaceId;
-    const workspace = await Workspace.findByPk(workspaceId);
 
-    if (!workspace) {
-      return res.status(404).json({ message: "Workspace not found" });
+    // Only check for access if the workspaceId parameter is present
+    if (workspaceId) {
+      const workspace = await Workspace.findByPk(workspaceId);
+
+      if (!workspace) {
+        return res.status(404).json({ message: "Workspace not found" });
+      }
+
+      // Check if the current user is a member or admin at the workspace
+      const workspaceMember = await WorkspaceMember.findOne({
+        where: {
+          workspaceId,
+          userId: req.currentUser.id,
+        },
+      });
+
+      if (!workspaceMember) {
+        return res.status(403).json({ message: "Access forbidden" });
+      }
+
+      if (role === "admin" && workspaceMember.role !== "admin") {
+        return res.status(403).json({ message: "Access forbidden" });
+      }
+
+      req.workspaceMember = workspaceMember;
+      req.workspace = workspace;
     }
 
-    // Check if the current user is a member or admin at the workspace
-    const workspaceMember = await WorkspaceMember.findOne({
-      where: {
-        workspaceId,
-        userId: req.currentUser.id,
-      },
-    });
-
-    if (!workspaceMember) {
-      return res.status(403).json({ message: "Access forbidden" });
-    }
-
-    if (role === "admin" && workspaceMember.role !== "admin") {
-      return res.status(403).json({ message: "Access forbidden" });
-    }
-
-    req.workspaceMember = workspaceMember;
-    req.workspace = workspace;
     next();
   });
 };
