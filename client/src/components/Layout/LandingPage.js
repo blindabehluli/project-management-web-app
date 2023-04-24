8e792b4e297fbd0ff58eb692a592983650e76592
getoarmorina3 <bb57230@ubt-uni.net>
2023-04-24 17:54:24 +0200

    small design changes
    Co-Authored-By: Getoar <78223720+getoarmorina3@users.noreply.github.com>

--- client/src/components/Layout/LandingPage.js
+++ client/src/components/Layout/LandingPage.js
@@ -7,18 +7,18 @@ const LandingPage = () => {
     <main className="flex justify-center items-center w-screen h-screen">
       <div className="container lg:px-28 px-6 md:px-12">
         <div className="flex flex-col justify-center align-center text-center">
-          <div className="text-[#303030] border border-dashed border-[#EAEAEA] p-8 break-words">
+          <div className="text-[#303030] border-y border-dashed border-[#EAEAEA] p-8 break-words">
             <h1
               className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-extrabold leading-tighter tracking-tight"
               data-aos="zoom-y-out"
             >
-              Collorate. Organize.{" "}
+              Collaborate. Organize.{" "}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 Manage.
               </span>
             </h1>
           </div>
-          <div className="border-x max-w-full border-[#EAEAEA] border-dashed p-10">
+          <div className="max-w-full p-10">
             <p className="text-[#A2A2A2] font-light text-xl md:text-2xl">
               Our project management app helps you stay
               <span className="text-[#111111]"> organized</span>, and on top of
@@ -30,7 +30,7 @@ const LandingPage = () => {
               ease.
             </p>
           </div>
-          <div className="flex justify-center border border-dashed border-[#EAEAEA]">
+          <div className="flex justify-center border-y border-dashed border-[#EAEAEA]">
             <div className="flex justify-center space-x-4 border-x border-dashed border-[#EAEAEA] p-10">
               <Link to="/signup">
                 <PrimaryButton>Get Started</PrimaryButton>
