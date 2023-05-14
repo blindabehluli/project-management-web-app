aaae69ea3e0cf50c447ae04ce1f9cf1105eed994
getoarmorina3 <bb57230@ubt-uni.net>
2023-05-14 22:30:04 +0200

    Update Forbidden.js

--- client/src/components/ErrorPages/Forbidden.js
+++ client/src/components/ErrorPages/Forbidden.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Forbidden = () => (
   <div className="flex flex-col justify-center items-center text-center h-screen">
     <h1 className="text-6xl lg:text-8xl font-extrabold text-black">Forbidden</h1>
-    <p className="mt-2 mx-4 text-[#A2A2A2] text-xl lg:text-2xl font-light">You are not authorized to access this page.</p>
+    <p className="mt-2 mx-4 text-[#A2A2A2] text-xl lg:text-2xl font-light">You are not authorized to make this request.</p>
   </div>
 );
 
